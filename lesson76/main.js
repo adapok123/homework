9f1e0a2df8029021270d878346d9217bfe0097e3
adapok123 <sudakov.y@mail.ru>
2024-08-23 15:03:32 +0700

    add lesson76

---
+++ lesson76/main.js
@@ -0,0 +1,27 @@
+
+const firstName = 'Yuriy';
+const  lastName = 'Sudakov';
+let age = '42';
+const myBirthday = '08.11.1981';
+const placeResidence = 'Krasnoyarck';
+let placeWork = 'military retiree';
+const  interests = 'Sport, book';
+const learning = 'web developer';
+
+
+
+
+console.log(firstName,lastName,age,'y.o.');
+console.log(myBirthday);
+console.log(placeResidence);
+console.log(placeWork);
+console.log(interests);
+console.log(learning);
+
+
+let admin, name;
+name = "Джон";
+
+admin = name;
+
+alert( admin );
