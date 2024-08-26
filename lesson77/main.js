3be6016a0e9c308fb93757e025b75c3a8520e36c
adapok123 <sudakov.y@mail.ru>
2024-08-26 15:55:55 +0700

    add lesson77

---
+++ lesson77/main.js
@@ -0,0 +1,43 @@
+const a = 5
+// + - * /
+const sum = 2 + 2
+console.log (sum)
+console.log (a)
+const minus = 10 - 5
+console.log (minus)
+const x = 20
+const y = 10
+const result = x * y
+console.log (result)
+const division = y / 2
+console.log(division)
+console.log(3 ** 2)
+console.log(5 % 2)
+console.log('Hello ' + 'World')
+const num = 2
+console.log(-num)
+
+
+
+
+const num0 = 5 + 10 * 3;
+console.log(num0)
+
+const num1 = 10;
+const num2 = 50;
+console.log(9 * num1)
+console.log(num2 / num1)
+console.log((num2 % 9) * num1)
+console.log(num2 + num1 / 8 + 2)
+
+
+let num3 = 4
+console.log(num3++)
+console.log(+num3)
+console.log(--num3)
+console.log(num3--)
+
+let number = 5
+console.log(++number)
+console.log(number++)
+console.log(number)
