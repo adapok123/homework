22a465e37983b0419ce29cd45f4484c7a3432ae7
adapok123 <sudakov.y@mail.ru>
2024-08-27 14:47:44 +0700

    add lesson78

---
+++ lesson78/main.js
@@ -0,0 +1,44 @@
+// String;
+// number;
+// Boolean;
+// undefined;
+// null;
+// bigInt;
+// symbol
+
+let instrument = 'инструмент' 
+const box = 'ящик' 
+
+console.log(`${instrument} лежит в ${box}е`)
+
+instrument = 10
+console.log(instrument)
+
+// true/false
+
+const user ='User'
+const age = '88'
+const isAdmin = true
+
+console.log(`${user} Admin: ${isAdmin}`)
+
+console.log(typeof(instrument))
+
+console.log(typeof(isAdmin))
+
+let foo
+console.log(foo)
+
+let sum = null
+console.log(sum)
+sum = 5 + 5
+console.log(sum)
+
+
+
+
+const bigInt = 1234567890123456789012345678901234567890n;
+
+console.log(bigInt)
+
+alert("Мы больше не работаем в выходные!")
