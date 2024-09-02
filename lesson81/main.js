57f84f5002a7fbd2bbc4e0bcd5691ddafaa161dd
adapok123 <sudakov.y@mail.ru>
2024-09-02 14:09:05 +0700

    add lesson81

---
+++ lesson81/main.js
@@ -0,0 +1,33 @@
+
+
+// const nambers = [1, 2, 3, 4, 5]
+
+// const nambers = [1, 2, true,[1,2,3,4,5], 5 ]
+
+
+// nambers[4] = 6
+
+
+// console.log(nambers)
+
+
+// const nambers = [1, 2, 3, 4, 5]
+
+
+// for (let i = 0; i < nambers.length; i++) {
+//      console.log(nambers[i] + 1)
+// }
+
+/*================================================Домашнее задание==================================================*/
+
+const nambers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (let i = 1; i <= 10; i = i + 2) {
+	console.log(nambers[i] + 0)
+}
+
+const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+for (let i = rainbow.length - 1; i >= 0; i--) {
+	console.log(rainbow[i])
+}
