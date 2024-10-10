dbd5974ec22c203805ea4d885e70155f50f9cf27
adapok123 <sudakov.y@mail.ru>
2024-10-10 11:22:23 +0700

    add lesson91

---
+++ lesson91/main.js
@@ -0,0 +1,23 @@
+// const btns = document.querySelectorAll('.btn')
+
+// btns.forEach(btn => {
+// btn.addEventListener('click',  () => {
+//     console.log(btn.textContent)
+// })
+// });
+
+
+const btnsWrapper = document.querySelector('.btns')
+
+
+
+btnsWrapper.addEventListener('click', event => {
+const target = event.target
+
+if(target && target.classList.contains('btn')) {
+
+    console.log(event.target.textContent)
+}
+
+
+})
