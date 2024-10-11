cdd707c200387bff6d6e0b1ad2ecc11bce404e35
adapok123 <sudakov.y@mail.ru>
2024-10-11 13:02:46 +0700

    Update main.js

--- lesson92 - 1/main.js
+++ lesson92 - 1/main.js
@@ -1,19 +1,31 @@
-const openModalBtn = document.getElementById('openModalBtn')
-const modal = document.getElementById('modal')
-const closeModalBtn = document.getElementById('closeModalBtn')
-const overlay = document.getElementById('overlay')
+const openModalBtn = document.getElementById('openModalBtn');
+    const modal = document.getElementById('modal');
+    const closeModalBtn = document.getElementById('closeModalBtn');
+    const overlay = document.getElementById('overlay');
+    const form = document.getElementById('myForm');
 
-openModalBtn.addEventListener('click', () => {
-	modal.style.display = 'block'
-	overlay.style.display = 'block'
-})
+    openModalBtn.addEventListener('click', () => {
+      modal.style.display = 'block';
+      overlay.style.display = 'block';
+    });
 
-closeModalBtn.addEventListener('click', () => {
-	modal.style.display = 'none'
-	overlay.style.display = 'none'
-})
+    closeModalBtn.addEventListener('click', () => {
+      modal.style.display = 'none';
+      overlay.style.display = 'none';
+    });
 
-overlay.addEventListener('click', () => {
-	modal.style.display = 'none'
-	overlay.style.display = 'none'
-})
+    overlay.addEventListener('click', () => {
+      modal.style.display = 'none';
+      overlay.style.display = 'none';
+    });
+
+    form.addEventListener('submit', (event) => {
+      event.preventDefault(); 
+
+      const name = document.getElementById('name').value;
+      const email = document.getElementById('email').value;
+
+      console.log('Имя:', name);
+      console.log('Email:', email);
+
+    });
